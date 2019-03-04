40079f581ce1529c123ed3fb67a909f73923c6c0
Anna Pettersen <aopettersen@gmail.com>
2019-03-04 16:03:26 +0000

    Added routes to rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :recipes, only: [:index, :show] do
+    resources :comments, only: :create
+    resources :quantities, only: [:new, :create]
+  end
+
+  resources :quantities, only: :destroy
+  resources :likes, only: [:create, :destroy]
+  resources :comments, only: :destroy
+  resources :countries, only: :index
 end
+
