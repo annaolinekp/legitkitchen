e75b3878508fc0c7d13d78fd37a9825485b3e0e9
Anna Pettersen <aopettersen@gmail.com>
2019-03-05 11:50:43 +0000

    Created seed file and countries#index

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,2 +1,11 @@
 class CountriesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index]
+
+  def index
+    @countries = Country.all
+  end
+
+  def show
+    @country = Country.find(params[:id])
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Recipe.destroy_all
+User.destroy_all
+Country.destroy_all
+
+puts 'Creating 10 fake users...'
+10.times do
+  user = User.new(
+    first_name: Faker::Name.unique.first_name,
+    last_name: Faker::Name.unique.last_name,
+    email: Faker::Internet.unique.email,
+    password: "123456"
+
+  )
+  user.save!
+
+  2.times do
+    recipe = Recipe.new(
+      name: Faker::Food.unique.dish,
+      description: Faker::Food.description,
+      category: Faker::Nation.nationality,
+      cooktime: "15min",
+      preptime: "30min",
+      picture_url: "none",
+      user: user
+
+    )
+    recipe.save!
+  end
+end
+
+puts 'Seeding countries...'
+50.times do
+  country = Country.new(
+    name: Faker::Address.unique.country
+  )
+  country.save!
+end
+
+puts "Finished!"
