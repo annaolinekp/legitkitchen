bd64fe003b71dfcf6dc3143780e2c0981107c7b6
Angela Inniss <Angela.inniss@hotmail.co.uk>
2019-03-07 16:15:51 +0000

    first draft of index page and started working in save controller

--- app/controllers/save_recipes_controller.rb
+++ app/controllers/save_recipes_controller.rb
@@ -1,4 +1,15 @@
 class SaveRecipesController < ApplicationController
   def create
+    # 1. find the recipe i want to save
+    @recipe = @recipe.find(params[:id])
+   # 2. find the id of the user (current user) create a new instance of save recipe
+    @save_recipe = Save_Recipe.new(user: current_user, recipe: @recipe)
+   # 3. save recipe
+   if @save_recipe.save
+    redirect_to recipe_path(@save_recipe.recipe)
+    else
+      render '/recipes'
+    end
+  # 4.
   end
 end
