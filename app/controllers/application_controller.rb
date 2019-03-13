3b0b818c9b537e84d15e9a21306283f7a17ebd52
Anna Pettersen <aopettersen@gmail.com>
2019-03-13 16:33:37 +0000

    added blurb and styled user show

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,10 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :profile_picture_url, :country_id])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :profile_picture_url, :country_id, :blurb])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :profile_picture_url, :country])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :profile_picture_url, :country, :blurb])
   end
 end
 
